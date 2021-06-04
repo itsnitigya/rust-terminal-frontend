b760b9632470fa84e52862da6b5f7bcbec32a7a6
Nitigya Kapoor <kapoornitigya@gmail.com>
2021-06-04 18:37:18 +0530

    change urls

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 import React from "react";
 import io from "socket.io-client";
 
-const socket = io("http://143.110.190.130");
+const socket = io("https://143.110.190.130");
 
 let heartbeatInterval;
 let heartbeatCount = 0;
